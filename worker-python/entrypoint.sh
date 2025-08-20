57445ff6737955b728ea9e7033a19bb079ccc168
GeTerow <gabriel.henrique.liebana@hotmail.com>
2025-08-20 10:17:58 -0300

    entrypoint

--- worker-python/entrypoint.sh
+++ worker-python/entrypoint.sh
@@ -6,7 +6,7 @@ cd /app || cd /usr/src/app || true
 
 echo "Diretório atual: $(pwd)"
 echo "Iniciando o servidor FastAPI (uvicorn) em background..."
-# garante app-dir explícito caso o cd falhe
+
 uvicorn --app-dir /app main:app --host 0.0.0.0 --port 8000 &
 
 echo "Iniciando o worker do Celery em foreground..."
